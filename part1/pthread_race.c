1ce6e5ab02c7dfce9842aeb3903ec013d41bc5d8
Cory <cjl9703@yahoo.com>
2019-04-30 22:05:04 -0700

    (modify) synchronized part1-threadracer

--- part1/pthread_race.c
+++ part1/pthread_race.c
@@ -14,18 +14,66 @@
 #define MAX_ITERATIONS 40
 /** END VALUES THAT CANNOT BE CHANGED */
 
-
 /**
  * use this struct as a parameter for the function
- * nanosleep. 
+ * nanosleep.
  * For exmaple : nanosleep(&ts, NULL);
  */
 struct timespec ts = {0, 123456 };
+int counter;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
+void* thread_func_add( void* arg ){
+    for(int i = 0; i < MAX_ITERATIONS; i++){
+        int idx = *((int *)arg);
+        pthread_mutex_lock(&mutex);
+        int temp = counter;
+        temp += 10;
+        counter = temp;
+        pthread_mutex_unlock(&mutex);
+        printf("Current Value written to Global Variables by ADDER thread id: %d is %d\n",idx,temp);
+        nanosleep(&ts, NULL);
+    }
+}
+void* thread_func_sub( void* arg ){
+    for(int i = 0; i < MAX_ITERATIONS; i++){
+        int idx = *((int *)arg);
+        pthread_mutex_lock(&mutex);
+        int temp = counter;
+        temp -= 10;
+        counter = temp;
+        pthread_mutex_unlock(&mutex);
+        printf("Current Value written to Global Variables by SUBTRACTOR thread id: %d is %d\n",idx,temp);
+        nanosleep(&ts, NULL);
+    }
+}
 
 int
 main(int argc, char** argv)
 {
+    counter = 0;
+
+    pthread_t ids[MAX_THREADS];
+    int i;
+
+    for(i = 0 ; i < MAX_THREADS ; i++)
+    {
+        int idx = i;
+        if(i%2 == 0){
+            //even
+            pthread_create(&ids[i],NULL,thread_func_add,(void *)&idx);
+        }else{
+            //odd
+            pthread_create(&ids[i],NULL,thread_func_sub,(void *)&idx);
+        }
+
+    }
+    for(i = 0; i < MAX_THREADS; i++)
+    {
+        pthread_join(ids[i],NULL);
+    }
+
+    printf("Final Value of Shared Variable : %d\n",counter);
 
     return 0;
 }
