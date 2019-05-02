15670e68d751739f95395bf5dcf7e23074270912
Cory <cjl9703@yahoo.com>
2019-05-02 00:53:00 -0700

    (add) Synchronization added, simple test for consume/produce added

--- part2/pandc.c
+++ part2/pandc.c
@@ -14,30 +14,61 @@ int X; //Number of items each producer will produce
 int Ptime; //(in seconds) sleep time of producer after producing an item
 int Ctime; //(in seconds) sleep time of each consumer after consuming an item
 
+//Mutex and Semaphores
+sem_t Full;
+sem_t Empty;
+pthread_mutex_t Mutex = PTHREAD_MUTEX_INITIALIZER;
+//flag
+int OverConsume;
+int ConsumeItemCount;
 
-/*
- * Function to remove item.
- * Item removed is returned
- */
 int dequeue_item()
 {
 
 }
 
-/*
- * Function to add item.
- * Item added is returned.
- * It is up to you to determine
- * how to use the ruturn value.
- * If you decide to not use it, then ignore
- * the return value, do not change the
- * return type to void.
- */
 int enqueue_item(int item)
 {
 
 }
+void* Producer( void* arg ){
+  struct timespec ts = {Ptime, 0 };
+    for(int i = 1; i <= X; i++){
+        int idx = *((int *)arg);
 
+        sem_wait(&Empty);
+        pthread_mutex_lock(&Mutex);
+        //critical section, add produced to buffer
+        printf("produce\n");
+        pthread_mutex_unlock(&Mutex);
+        sem_post(&Full);
+        nanosleep(&ts, NULL);
+    }
+}
+void* Consumer( void* arg ){
+  struct timespec ts = {Ctime, 0 };
+  int idx = *((int *)arg);
+    for(int i = 1; i <= ConsumeItemCount; i++){
+        sem_wait(&Full);
+        pthread_mutex_lock(&Mutex);
+        //critical section, remove item from buffer
+        printf("consume\n");
+        pthread_mutex_unlock(&Mutex);
+        sem_post(&Empty);
+        nanosleep(&ts, NULL);
+    }
+    //If OverConsume and last thread then consume one more item
+    if(idx == C && OverConsume > 0){
+      sem_wait(&Full);
+      pthread_mutex_lock(&Mutex);
+      //critical section, remove item from buffer
+      pthread_mutex_unlock(&Mutex);
+      sem_post(&Empty);
+      nanosleep(&ts, NULL);
+    }
+}
+
+//====================== PRINTING ===================================
 void displayTimestamp(){
   time_t t = time(NULL);
   struct tm tm = *localtime(&t);
@@ -55,14 +86,13 @@ void displayTableOfValues(int n, int p, int c, int x, int ptime, int ctime){
   printf("       Time each Consumer Sleeps (seconds) : %d\n",ctime);
 }
 void displayInvalidInput(){
-  printf("Incorrect parameters.\n");
+  printf("Not Enough Arguments Entered.\n");
   printf("Example valid command: ./pandc 7 7 9 15 1 1 \n");
   printf("Your output will contain a table similar to: \n");
   displayTableOfValues(7,7,9,15,1,1);
 }
 
 int main(int argc, char** argv) {
-
   displayTimestamp();
 
   if(argc >= 7){
@@ -73,8 +103,42 @@ int main(int argc, char** argv) {
     X = atoi(argv[4]);
     Ptime = atoi(argv[5]);
     Ctime = atoi(argv[6]);
-
     displayTableOfValues(N,P,C,X,Ptime,Ctime);
+
+    //init consumer values
+    ConsumeItemCount = (P*X)/C;
+    OverConsume = (P*X)%C != 0;
+
+    //init synchronization
+    if(sem_init(&Full, 0, 0) < 0){
+      perror("sem_init failed to initialize Full\n");
+    }
+    if(sem_init(&Empty, 0, N)){
+      perror("sem_init failed to initialize Empty");
+    }
+    //Producer Threads
+    pthread_t p_ids[P];
+    for(int i = 0 ; i < P ; i++){
+        int idx = i+1;
+        pthread_create(&p_ids[i],NULL,Producer,(void *)&idx);
+    }
+    //Consumer Threads
+    pthread_t c_ids[C];
+    for(int i = 0; i < C; i++){
+      int idx = i+1;
+      pthread_create(&c_ids[i],NULL,Consumer,(void *)&idx);
+    }
+    //Join Producers
+    for(int i = 0; i < P; i++)
+    {
+        pthread_join(p_ids[i],NULL);
+    }
+    //Join Consumers
+    for(int i = 0; i < C; i++)
+    {
+        pthread_join(c_ids[i],NULL);
+    }
+
   }else{
     displayInvalidInput();
   }
